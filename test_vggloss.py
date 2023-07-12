e26c9437b7126b7422e34f9d8477df9a4a93cd47
ryushinn <chenliu.99@outlook.com>
2023-07-12 22:39:43 +0100

    * add unit test of stats loss

---
+++ test_vggloss.py
@@ -0,0 +1,131 @@
+import argparse
+import os
+import time
+from datetime import datetime
+import random
+import numpy as np
+
+import os.path as opath
+import copy
+import torch
+import torch.optim as optim
+import torchvision.io as tvio
+import torchvision.datasets as dset
+import torchvision.transforms as tforms
+from torchvision.utils import save_image
+
+import matplotlib.pyplot as plt
+from tqdm import tqdm
+
+import torch.nn as nn
+import net
+import metrics
+import lib.layers as layers
+import lib.utils as utils
+import lib.odenvp as odenvp
+import lib.multiscale_parallel as multiscale_parallel
+
+parser = argparse.ArgumentParser("ODE texture")
+
+parser.add_argument("--exemplar_path", type=str)
+parser.add_argument("--exp_path", type=str)
+parser.add_argument("--lr", type=float, default=1e-4)
+
+parser.add_argument("--num_epochs", type=int, default=1000)
+parser.add_argument("--num_disp_epochs", type=int, default=10)
+# args
+args = parser.parse_args()
+
+
+def seed_all(seed):
+    """
+    provide the seed for reproducibility
+    """
+    random.seed(seed)
+    os.environ["PYTHONHASHSEED"] = str(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+
+
+if __name__ == "__main__":
+    # pre, reproducible
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    cvt = lambda x: x.to(device)
+    torch.set_default_dtype(torch.float32)
+
+    seed_all(42)
+
+    # data
+    if not opath.exists(args.exemplar_path):
+        raise ValueError(f"There is not file in {args.exemplar_path}")
+
+    ## create workspace to store results
+    workspace = datetime.now().strftime("%Y_%m_%d_%H_%M_%S")
+    ws_path = opath.join(args.exp_path, workspace)
+    os.makedirs(ws_path, exist_ok=True)
+
+    read_tforms = [
+        # tforms.RandomCrop(128),
+        tforms.ConvertImageDtype(torch.float32),
+    ]
+
+    exemplar = tvio.read_image(args.exemplar_path)
+    for tform in read_tforms:
+        exemplar = tform(exemplar)
+
+    ## write transformed exemplar
+    tvio.write_png(
+        tforms.ConvertImageDtype(torch.uint8)(exemplar),
+        opath.join(ws_path, "exemplar.png"),
+    )
+
+    exemplar = cvt(exemplar)
+    data_shape = exemplar.size()
+    exemplar = torch.unsqueeze(exemplar, 0)  # add the batch dim
+
+    # model
+
+    noise = torch.randn(1, 3, 256, 256).to(device)
+    noise.requires_grad_(True)
+    # training preconfig
+
+    ## VGG features
+    features = metrics.VGGFeatures().to(device)
+    gmatrices_exemplar = list(map(metrics.GramMatrix, features(exemplar)))
+    loss_fn = nn.MSELoss(reduction="mean")
+
+    optimizer = optim.Adam([noise], lr=args.lr)
+    loss_meter = utils.RunningAverageMeter(0.97)
+
+    # training procedure
+    with tqdm(total=args.num_epochs, desc="Epoch") as t:
+        for ep in range(args.num_epochs):
+            optimizer.zero_grad()
+
+            ## compute gram matrices
+            gmatrices_samples = list(
+                map(metrics.GramMatrix, features(torch.sigmoid(noise)))
+            )
+
+            ## compute the gradients
+            loss = 0.0
+            for gmatrix_e, gmatrix_s in zip(gmatrices_exemplar, gmatrices_samples):
+                loss += loss_fn(gmatrix_e.expand_as(gmatrix_s), gmatrix_s)
+
+            loss.backward()
+
+            optimizer.step()
+
+            loss_meter.update(loss.item())
+
+            if ep % args.num_disp_epochs == 0:
+                with torch.no_grad():
+                    tvio.write_png(
+                        tforms.ConvertImageDtype(torch.uint8)(torch.sigmoid(noise)[0].to('cpu')),
+                        opath.join(ws_path, f"noise_{ep}.png"),
+                    )
+
+            t.set_postfix({"running_loss": f"{loss_meter.avg}"})
+            t.update()
+            ## test in some period
