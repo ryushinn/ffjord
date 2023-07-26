f2f855546e4eb2609bbccfb1a5179479ae596d92
ryushinn <chenliu.99@outlook.com>
2023-07-26 15:56:13 +0100

    * use more vgg layers for loss

--- metrics.py
+++ metrics.py
@@ -9,23 +9,9 @@ class VGGFeatures(nn.Module):
         super(VGGFeatures, self).__init__()
 
         vgg_pretrained_features = vgg19(pretrained=True).features
+        
+        self.features = vgg_pretrained_features
 
-        self.slice1 = nn.Sequential()
-        self.slice2 = nn.Sequential()
-        self.slice3 = nn.Sequential()
-        self.slice4 = nn.Sequential()
-        self.slice5 = nn.Sequential()
-
-        for x in range(4):  # relu_1_1
-            self.slice1.add_module(str(x), vgg_pretrained_features[x])
-        for x in range(4, 9):  # relu_2_1
-            self.slice2.add_module(str(x), vgg_pretrained_features[x])
-        for x in range(9, 18):  # relu_3_1
-            self.slice3.add_module(str(x), vgg_pretrained_features[x])
-        for x in range(18, 27):  # relu_4_1
-            self.slice4.add_module(str(x), vgg_pretrained_features[x])
-        for x in range(27, 36):  # relu_5_1
-            self.slice5.add_module(str(x), vgg_pretrained_features[x])
         for param in self.parameters():
             param.requires_grad = False
 
@@ -33,25 +19,29 @@ class VGGFeatures(nn.Module):
         ## normalize
         mean = [0.485, 0.456, 0.406]
         std = [0.229, 0.224, 0.225]
-        device = "cuda" if x.is_cuda else "cpu"
+        device = 'cuda' if x.is_cuda else 'cpu'
         mean = torch.as_tensor(mean, device=device).view(1, 3, 1, 1)
         std = torch.as_tensor(std, device=device).view(1, 3, 1, 1)
         x = x.sub(mean)
         x = x.div(std)
 
         # get features
-        h1 = self.slice1(x)
-        h_relu1_1 = h1
-        h2 = self.slice2(h1)
-        h_relu2_1 = h2
-        h3 = self.slice3(h2)
-        h_relu3_1 = h3
-        h4 = self.slice4(h3)
-        h_relu4_1 = h4
-        h5 = self.slice5(h4)
-        h_relu5_1 = h5
+        important_layers = [
+            1, 3, # block1_conv1, 2
+            6, 8, # block2_conv1, 2
+            11, 13, 15, 17, # block3_conv1, 2, 3, 4
+            20, 22, 24, 26, # block4_conv1, 2, 3, 4
+            29, 31, 33, 35, # block5_conv1, 2, 3, 4
+        ]
 
-        return [h_relu1_1, h_relu2_1, h_relu3_1, h_relu4_1, h_relu5_1]
+        features_layers = []
+        for i in range(len(self.features)):
+            x = self.features[i](x)
+            
+            if i in important_layers:
+                features_layers.append(x)
+
+        return features_layers
 
 
 def GramMatrix(input):
